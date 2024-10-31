25b8c45a3b6b04d1ab865d61478ea87d0d9cbcfa
EXA72224 <jperez@cfotechlatam.com>
2024-10-30 22:22:26 -0300

    Made some general changes to FileUploader and dadded a constants file

--- src/components/FileUploader/FileUploader.tsx
+++ src/components/FileUploader/FileUploader.tsx
@@ -3,12 +3,11 @@ import { MdCloudUpload } from 'react-icons/md'
 import { fileStore } from '../../store/useStore'
 import DragOverlay from '../DragOverlay'
 import { twMerge } from 'tailwind-merge'
+import { maxSizeInBytes } from '../../models/constants'
 
-interface Props {}
-
-const FileUploader = ({}: Props) => {
+const FileUploader = () => {
   const { addFiles } = fileStore()
-  const handleDropItem = (acceptedFiles: File[]) => {
+  const handleAddingItems = (acceptedFiles: File[]) => {
     addFiles(acceptedFiles)
   }
 
@@ -18,39 +17,24 @@ const FileUploader = ({}: Props) => {
       'image/jpeg': ['.jpg', '.jpeg'],
       'application/pdf': ['.pdf']
     },
-    // maxSize: 100_000_000, // bytes = 100 mb
-    maxSize: 500_000, // bytes = 5 mb
-    onDrop: handleDropItem,
-    // onDropRejected: (fileRejections) => {
-    //   console.log(fileRejections)
-    //   const setErrorMessage = (): string => {
-    //     if(fileRejections[0].errors[0].code === 'file-invalid-type') {
-    //       return 'Some file type is not supported'
-    //     } else if (fileRejections[0].errors[0].code === 'file-too-large') {
-    //       return 'Some file exceeds the total size limit'
-    //     } else {
-    //       return 'Something went wrong'
-    //     }
-    //   }
-    //   openNotification({
-    //     message: setErrorMessage(),
-    //     id: 'notification-upload-error-rejected',
-    //     status: 'error',
-    //     duration: 5000
-    //   })
-    // }
+    maxSize: maxSizeInBytes,
+    onDrop: handleAddingItems,
+    onDropRejected: (fileRejections) => {
+      const errorFiles = fileRejections.map( rejected => rejected.file)
+      handleAddingItems(errorFiles)
+    }
   })
 
   return (
     <>
-      <section className='mx-auto my-10 w-[80%] flex content-center items-center'>
+      <section className='mx-auto my-10 w-[80%] flex content-center items-center group'>
         <button
-          className='flex min-h-60 w-full border-spacing-2 select-none flex-col items-center justify-center gap-6 rounded-xl border-2 border-slate-500/40 bg-slate-100 dark:bg-slate-700/30 text-slate-800 dark:text-slate-100 outline-none transition-colors duration-150 hover:border-slate-500/70 focus:border-slate-500 active:border-slate-500 z-30'
+          className='flex min-h-60 w-full border-spacing-2 select-none flex-col items-center justify-center gap-6 rounded-xl border-2 border-slate-500/40 bg-slate-100 dark:bg-slate-700/30 text-slate-800 dark:text-slate-100 outline-none transition-colors duration-150 hover:border-slate-500/70 focus:border-slate-500 active:border-blue-500 z-30'
           {...getDropzoneProps()}
           aria-label='Drop zone'
         >
           <input {...getInputProps()} />
-          <MdCloudUpload className={twMerge(` text-slate-400/80 dark:text-slate-300/50 transition-transform duration-200`, isDragActive ? 'animate-bounce text-8xl' : 'text-5xl')} />
+          <MdCloudUpload className={twMerge(`group-hover:scale-150 group-hover:text-blue-500 text-slate-400/80 dark:text-slate-300/50 transition-transform duration-200`, isDragActive ? 'animate-bounce text-8xl text-blue-500 dark:text-blue-500' : 'text-5xl')} />
           <div className='flex flex-col gap-1'>
             { isDragActive
               ?
@@ -60,7 +44,7 @@ const FileUploader = ({}: Props) => {
                 <span>
                   <b>Drop</b> items here or <b>click</b> to browse files
                 </span>
-                <span className='font-thin tracking-wide'>Max 1gb. Only pdf & png/jpg.</span>
+                <span className='font-thin tracking-wide'>Max {maxSizeInBytes/1000}mb. Only pdf & png/jpg.</span>
               </>
             }
           </div>
---
+++ src/models/constants.ts
@@ -0,0 +1,2 @@
+// export const maxSizeInBytes = 100_000 // bytes = 100 mb
+export const maxSizeInBytes = 100_000_000 // bytes = 1gb
